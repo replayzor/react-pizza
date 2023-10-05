57efcd910dd1eaf72d68477c63075a5657fb8111
replayzor <oltean.ionut1989@gmail.com>
2023-10-05 16:56:17 +0300

    feat. add Error to Applayout

--- src/App.jsx
+++ src/App.jsx
@@ -5,47 +5,47 @@ import Error from "./ui/Error";
 import Menu, { loader as menuLoader } from "./components/menu/Menu";
 import Cart from "./components/cart/Cart";
 import CreateOrder, {
-	action as createOrderAction,
+  action as createOrderAction,
 } from "./components/order/CreateOrder";
 import Order, { loader as orderLoader } from "./components/order/Order";
 import AppLayout from "./ui/AppLayout";
 
 const router = createBrowserRouter([
-	{
-		element: <AppLayout />,
-
-		children: [
-			{
-				path: "/",
-				element: <Home />,
-			},
-			{
-				path: "menu",
-				element: <Menu />,
-				loader: menuLoader,
-				errorElement: <Error />,
-			},
-			{
-				path: "cart",
-				element: <Cart />,
-			},
-			{
-				path: "order/new",
-				element: <CreateOrder />,
-				action: createOrderAction,
-			},
-			{
-				path: "order/:orderId",
-				element: <Order />,
-				loader: orderLoader,
-				errorElement: <Error />,
-			},
-		],
-	},
+  {
+    element: <AppLayout />,
+    errorElement: <Error />,
+    children: [
+      {
+        path: "/",
+        element: <Home />,
+      },
+      {
+        path: "menu",
+        element: <Menu />,
+        loader: menuLoader,
+        errorElement: <Error />,
+      },
+      {
+        path: "cart",
+        element: <Cart />,
+      },
+      {
+        path: "order/new",
+        element: <CreateOrder />,
+        action: createOrderAction,
+      },
+      {
+        path: "order/:orderId",
+        element: <Order />,
+        loader: orderLoader,
+        errorElement: <Error />,
+      },
+    ],
+  },
 ]);
 
 function App() {
-	return <RouterProvider router={router} />;
+  return <RouterProvider router={router} />;
 }
 
 export default App;
