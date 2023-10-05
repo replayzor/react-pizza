5e9060f1bd4c1bd97bfcbf4d9d9b3eb5cffa7e05
replayzor <oltean.ionut1989@gmail.com>
2023-10-05 11:12:00 +0300

    feat. add css

--- src/components/cart/CartOverview.jsx
+++ src/components/cart/CartOverview.jsx
@@ -1,15 +1,15 @@
 import { Link } from "react-router-dom";
 
 function CartOverview() {
-	return (
-		<div>
-			<p>
-				<span>23 pizzas</span>
-				<span>$23.45</span>
-			</p>
-			<Link to="cart">Open cart &rarr;</Link>
-		</div>
-	);
+  return (
+    <div className="bg-stone-800 p-4 uppercase text-slate-200">
+      <p className="space-x-4 font-semibold text-stone-300">
+        <span>23 pizzas</span>
+        <span>$23.45</span>
+      </p>
+      <Link to="cart">Open cart &rarr;</Link>
+    </div>
+  );
 }
 
 export default CartOverview;
--- src/ui/Header.jsx
+++ src/ui/Header.jsx
@@ -1,13 +1,16 @@
 import { Link } from "react-router-dom";
 import SearchOrder from "../components/order/SearchOrder";
+import Username from "../components/user/Username";
 
 function Header() {
-	return (
-		<header>
-			<Link to="/">React Pizza</Link>
-			<SearchOrder />
-			<p>replay</p>
-		</header>
-	);
+  return (
+    <header className="flex justify-between border-b border-stone-200 bg-yellow-500 px-4 py-3 uppercase">
+      <Link to="/" className="tracking-widest">
+        React Pizza
+      </Link>
+      <SearchOrder />
+      <Username />
+    </header>
+  );
 }
 export default Header;
