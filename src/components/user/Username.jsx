3fdb281a754dd2d02c8b7f0362c82b46906a91f9
replayzor <oltean.ionut1989@gmail.com>
2023-10-11 13:38:54 +0300

    Improved Username component with username context integration

--- src/components/user/Username.jsx
+++ src/components/user/Username.jsx
@@ -1,4 +1,12 @@
+import { useAppContext } from "../../context/AppProvider";
+
 function Username() {
-  return <div className="hidden text-sm font-semibold md:block">replay</div>;
+  const { username } = useAppContext();
+
+  if (!username) return null;
+
+  return (
+    <div className="hidden text-sm font-semibold md:block">{username}</div>
+  );
 }
 export default Username;
