18b3d5aed21634944fe3f8a587f9cf8977fbcfd3
replayzor <oltean.ionut1989@gmail.com>
2023-10-11 13:43:16 +0300

    Improved Home component with user interaction

--- src/ui/Home.jsx
+++ src/ui/Home.jsx
@@ -1,6 +1,9 @@
 import CreateUser from "../components/user/CreateUser";
+import { useAppContext } from "../context/AppProvider";
+import Button from "./Button";
 
 function Home() {
+  const { username } = useAppContext();
   return (
     <div className="mx-4 my-10 text-center sm:my-16">
       <h1 className="mb-8 text-xl font-semibold md:text-3xl">
@@ -10,8 +13,13 @@ function Home() {
           Straight out of the oven, straight to you.
         </span>
       </h1>
-
-      <CreateUser />
+      {username === "" ? (
+        <CreateUser />
+      ) : (
+        <Button type="primary" to="/menu">
+          Continue ordering, {username}
+        </Button>
+      )}
     </div>
   );
 }
